99e205b140f2628f6d3ef281355d3efcb053a4ac
Dmytro Skaliun <dmytro.skaliun@capgemini.com>
2025-02-08 17:53:14 +0100

    Final Solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,55 @@ import './App.css'
 
 function PhoneNumberInput() {
   const [phoneNumber, setPhoneNumber] = useState('');
+  const [inputErrorMsg, setInputErrorMsg] = useState('');
 
   function handleChange(input: string) {
-    if (!input || !input.trim()) {
-      return;
+    let timer = null;
+    if (timer) {
+      clearTimeout(timer);
+      setInputErrorMsg('');
     }
 
-    let formattedInput = input.trim();
+    if (!input || !input.trim()) {
+      setPhoneNumber('')
+      return null;
+    }
 
+    let formattedInput = input.replace(/\)/g, '').replace(/\(/g, '').replace(/-/g, '').replace(/ /g, '');
+
+    if (isNaN(Number(formattedInput))) {
+      return null;
+    }
+
+    if (formattedInput.length > 10) {
+      setInputErrorMsg('Max phone number length: 10 digits!');
+      timer = setTimeout(() => {
+        setInputErrorMsg('');
+      }, 3000);
+
+      return null;
+    }
+
+    // phone number example: (999)1234-123
+    if (formattedInput.length >= 8) {
+      // 123 4567 8
+      formattedInput = '( ' + formattedInput.substring(0, 3) + ' ) ' + formattedInput.substring(3, 7) + ' - ' + formattedInput.substring(7);
+    } else if (formattedInput.length >= 4) {
+      // 123 4
+      formattedInput = '( ' + formattedInput.substring(0, 3) + ' ) ' + formattedInput.substring(3, 7);
+    } else {
+      // 12
+      formattedInput = formattedInput.length > 0 ? '( ' + formattedInput : '';
+    }
+
+    setPhoneNumber(formattedInput)
   }
 
 
   return <>
     <label>Phobe Number</label>
     <input type="tel" onChange={(e) => handleChange(e.target.value)} value={phoneNumber} />
+    <div className='invalid-input-message'>{inputErrorMsg}</div>
   </>
 }
 
